43c88e63e521b3bb0f20f76081bf34a511de7e12
Arvindh Kumar Chandran <arvindhium@gmail.com>
2018-12-17 20:40:23 -0800

    Created a main program to call all the modules

---
+++ brain.py
@@ -0,0 +1,11 @@
+from red_crawler import red_crawler
+from image_resizer import image_resizer
+from change_wall import change_wall
+
+def main():
+    red_crawler()
+    image_resizer()
+    change_wall()
+
+if __name__ == "__main__":
+    main()
