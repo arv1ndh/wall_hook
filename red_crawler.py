bbd4ef4be15edcf92cb879fff1b467b0a811093f
Arvindh Kumar Chandran <arvindhium@gmail.com>
2019-01-29 09:21:38 -0800

    Added regex to parse the resolution of all images and choose the highest one, should work on removing the a tags hardcoded extraction

--- red_crawler.py
+++ red_crawler.py
@@ -2,27 +2,48 @@ from html.parser import HTMLParser
 from datetime import datetime
 from urllib.request import urlopen,Request
 import os
+import re
 
 custom_header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+REQ_IMG_H = 1080
+REQ_IMG_W = 1920
 
 class RedditEpParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.post_s_flag = 0
+        self.img_flag = 0
         self.link = ""
+        self.regex = re.compile("[\[,\(]\d*x\d*[\],\)]")
+        self.final_link = ""
 
     def handle_starttag(self, tag, attrs):
-        if 'img' == tag and len(attrs):
-            if attrs[0][0] == 'src' and 'renderTimingPixel' in attrs[0][1]:
-                self.post_s_flag = 1
+        if 'h2' == tag and len(attrs) and "imors3-" in attrs[0][1]:
+            self.post_s_flag = 1
 
         if 'a' == tag and self.post_s_flag == 1:
-            if attrs[0][1].startswith("/r") and "comments" in attrs[0][1]:
-                if not len(self.link):
-                    self.link = attrs[0][1]
+            if len(attrs) > 2 and attrs[2][1].startswith("/r") and "comments" in attrs[2][1]:
+                self.link = attrs[2][1]
             elif attrs[0][1].endswith("jpg") or attrs[0][1].endswith("png"):
-                if not len(self.link):
-                    self.link = attrs[0][1]
+                print(attrs)
+                self.link = attrs[0][1]
+                self.img_flag = 1
+
+    def handle_data(self, data):
+        global REQ_IMG_H, REQ_IMG_W
+        if self.link != "" and self.img_flag == 0:
+            result = self.regex.search(data)
+            if result is None:
+                self.link = ""
+                return
+            resol_string = result.group()
+            resol_string = resol_string[1:-1]
+            width, height = map(int, resol_string.split('x'))
+            if width >= height and height >= REQ_IMG_H and width >= REQ_IMG_W:
+                REQ_IMG_H = height
+                REQ_IMG_W = width
+                self.final_link = self.link
+            self.link = ""
 
 def red_crawler():
     red_url = "https://www.reddit.com"
@@ -34,7 +55,11 @@ def red_crawler():
 
     red_parser_obj = RedditEpParser()
     red_parser_obj.feed(html_page)
-    post_link = red_parser_obj.link
+    post_link = red_parser_obj.final_link
+    if post_link is None:
+        print("No high res image found, exiting")
+        import sys
+        sys.exit()
     print("Obtained the post link ", post_link)
 
     red_parser_obj = RedditEpParser()
@@ -54,6 +79,8 @@ def red_crawler():
     print("Image Downloaded successfully")
     return img_name
 
+def main():
+    red_crawler()
 
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
