9f835ec2019ac0070c2f8d2abbd73e1a4c418121
Arvindh Kumar Chandran <arvindhium@gmail.com>
2019-03-01 15:15:39 -0800

    Made the parser more robust

--- red_crawler.py
+++ red_crawler.py
@@ -18,15 +18,12 @@ class RedditEpParser(HTMLParser):
         self.final_link = ""
 
     def handle_starttag(self, tag, attrs):
-        if 'h2' == tag and len(attrs) and "imors3-" in attrs[0][1]:
-            self.post_s_flag = 1
-
-        if 'a' == tag and self.post_s_flag == 1:
-            if len(attrs) > 2 and attrs[2][1].startswith("/r") and "comments" in attrs[2][1]:
-                self.link = attrs[2][1]
-            elif attrs[0][1].endswith("jpg") or attrs[0][1].endswith("png"):
-                print(attrs)
-                self.link = attrs[0][1]
+        if 'a' == tag and len(attrs) > 2 and ("data-click-id","body") in attrs:
+            t_link = dict(attrs)["href"]
+            if "EarthPorn/comments" in t_link:
+                self.link = t_link
+            elif t_link.endswith("jpg") or t_link.endswith("png"):
+                self.link = t_link
                 self.img_flag = 1
 
     def handle_data(self, data):
@@ -45,6 +42,25 @@ class RedditEpParser(HTMLParser):
                 self.final_link = self.link
             self.link = ""
 
+class ImgParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.img_rend_flag = 0
+        self.link = ""
+
+    def handle_starttag(self, tag, attrs):
+        if 'img' == tag:
+            ch_img = dict(attrs)["src"]
+            if ch_img.endswith("renderTimingPixel.png"):
+                self.img_rend_flag = 1
+
+        if 'a' == tag and self.img_rend_flag == 1:
+            t_dict = dict(attrs)
+            if (t_dict["href"].endswith("png") or t_dict["href"].endswith("jpg")):
+                self.link = t_dict["href"]
+                self.img_rend_flag = 0
+
+
 def red_crawler():
     red_url = "https://www.reddit.com"
     categ = "/r/EarthPorn"
@@ -56,21 +72,22 @@ def red_crawler():
     red_parser_obj = RedditEpParser()
     red_parser_obj.feed(html_page)
     post_link = red_parser_obj.final_link
-    if post_link is None:
+    if len(post_link) == 0:
         print("No high res image found, exiting")
         import sys
         sys.exit()
     print("Obtained the post link ", post_link)
 
-    red_parser_obj = RedditEpParser()
+    redimg_parser_obj = ImgParser()
     img_url = red_url + post_link
     print("Trying to fetch ", img_url)
     url_obj = urlopen(Request(img_url, headers = custom_header))
     html_page = str(url_obj.read())
 
-    red_parser_obj.feed(html_page)
-    img_link = red_parser_obj.link
+    redimg_parser_obj.feed(html_page)
+    img_link = redimg_parser_obj.link
     print("Trying to download ", img_link)
+    print(img_link)
     img_link_obj = urlopen(Request(img_link, headers = custom_header))
 
     img_name = "todays_" + img_link.split('/')[-1]
